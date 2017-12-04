e9ed4b2ada24550c0c12c7f87e9cedad75458c03
lshannon <luke.shannon@gmail.com>
2017-12-03 18:38:58 -0800

    Updates to local testing scripts

--- local_scripts/gfsh.sh
+++ local_scripts/gfsh.sh
@@ -2,7 +2,7 @@
 ##############################################################################
 # Runs locally to start 'gfsh' to manage the local cluster
 ##############################################################################
-source setenv.sh
+source set_env.sh
 echo "Gemfire Shell (gfsh) is command-line interface to launch, manage and monitor Gemfire processes"
 echo "Type connect to connect to the grid if its running"
 gfsh
