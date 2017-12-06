88243b70c0420d272fd152cc7b25ba53d21f2a0d
lshannon <luke.shannon@gmail.com>
2017-12-05 19:58:54 -0800

    Working on remote scripts

--- remote_management_scripts/stop_cluster.sh
+++ remote_management_scripts/stop_cluster.sh
@@ -6,6 +6,5 @@ cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source set_env.sh
 LOCATOR_1=$(sed -n '1p' < locators.txt)
 echo "To Shutdown the cluster run the following commands:"
-echo "connect --locator=$LOCATOR_1[10334]"
-echo "shutdown --include-locators=true"
-gfsh
+
+gfsh -e "connect --locator=$LOCATOR_1[10334]" -e "shutdown --include-locators"
